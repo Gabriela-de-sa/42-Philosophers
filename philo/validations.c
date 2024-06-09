33108738c728656ad62bdb83c030d3d65933d0fd
Gabriela De Sa <gde-sa@c1r2p1.42sp.org.br>
2024-06-09 17:08:30 -0300

    Makefile feito

---
+++ philo/validations.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validations.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gde-sa <gde-sa@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/09 16:57:21 by gde-sa            #+#    #+#             */
+/*   Updated: 2024/06/09 16:59:05 by gde-sa           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "include/philo.h"
+
+int	ft_check_args(int argc, char **argv)
+{
+	if (argc < 5 || argc > 6)
+		return (1);
+	// somente numero en todos os argumentos tirando o exe
+	
+}
